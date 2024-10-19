2dce6561e7c0f2f1beb5ccb9af8ddddb1de701e8
Dhia aissa <dhia.aissa@esprit.tn>
2024-10-19 01:11:03 +0100

    test2 skier/

--- src/test/java/tn/esprit/spring/SkierServicesMockitoTest.java
+++ src/test/java/tn/esprit/spring/SkierServicesMockitoTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -12,7 +13,6 @@ import tn.esprit.spring.services.*;
 import java.time.LocalDate;
 import java.util.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class SkierServicesMockitoTest {
@@ -25,16 +25,26 @@ public class SkierServicesMockitoTest {
     private ICourseRepository courseRepository;
     @Mock
     private IRegistrationRepository registrationRepository;
+
+    private AutoCloseable closeable; // To manage resources for openMocks
+
     @Before
     public void init() {
-        MockitoAnnotations.initMocks(this);
+        // Use openMocks instead of initMocks and close resources after test
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    // Make sure to close mocks after the test execution
+    @After
+    public void tearDown() throws Exception {
+        closeable.close();
     }
 
     @Test
     public void testRetrieveAllSkiers() {
         List<Skier> skiers = new ArrayList<>();
-        skiers.add(new Skier(1L, "dhia", "aissa", LocalDate.now(),"Zarzis", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>()));
-        skiers.add(new Skier(2L, "wassim", "makther", LocalDate.now(),"Medenine", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>()));
+        skiers.add(new Skier(1L, "dhia", "aissa", LocalDate.now(), "Zarzis", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>()));
+        skiers.add(new Skier(2L, "wassim", "makther", LocalDate.now(), "Medenine", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>()));
         when(skierRepository.findAll()).thenReturn(skiers);
         List<Skier> result = skierServices.retrieveAllSkiers();
         assertEquals(skiers, result);
@@ -42,7 +52,7 @@ public class SkierServicesMockitoTest {
 
     @Test
     public void testAddSkier() {
-        Skier skier = new Skier(3L,"idriss", "el bessi", LocalDate.now(),"Ariana", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>());
+        Skier skier = new Skier(3L, "idriss", "el bessi", LocalDate.now(), "Ariana", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>());
         when(skierRepository.save(skier)).thenReturn(skier);
         Skier result = skierServices.addSkier(skier);
         assertEquals(skier, result);
@@ -51,10 +61,9 @@ public class SkierServicesMockitoTest {
     @Test
     public void testRetrieveSkier() {
         Long skierId = 4L;
-        Skier skier = new Skier(4L,"hamza", "zaghwen", LocalDate.now(),"Tunis", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>());
+        Skier skier = new Skier(4L, "hamza", "zaghwen", LocalDate.now(), "Tunis", new Subscription(), new HashSet<Piste>(), new HashSet<Registration>());
         when(skierRepository.findById(skierId)).thenReturn(Optional.of(skier));
         Skier result = skierServices.retrieveSkier(skierId);
         assertEquals(skier, result);
     }
-
 }
