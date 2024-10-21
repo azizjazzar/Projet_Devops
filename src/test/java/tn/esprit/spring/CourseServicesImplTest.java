146c82bade6a5d2bb1cc5977cf88e97199bc0de7
ademnsir <nsir.adem@esprit.tn>
2024-10-21 10:59:02 +0100

    pom

--- src/test/java/tn/esprit/spring/CourseServicesImplTest.java
+++ src/test/java/tn/esprit/spring/CourseServicesImplTest.java
@@ -29,39 +29,42 @@ class CourseServicesImplTest {
 
 
 
-    
+
+    // Test de la méthode recommendCoursesForUser
     @Test
-    public void testRecommendCoursesForUser() {
+     void testRecommendCoursesForUser() {
         Course course1 = new Course();
-        course1.setLevel(1); 
+        course1.setLevel(1); // Niveau débutant
 
         Course course2 = new Course();
-        course2.setLevel(3); 
+        course2.setLevel(3); // Niveau avancé
 
         when(courseRepository.findAll()).thenReturn(Arrays.asList(course1, course2));
 
+        // Cas où l'utilisateur a un niveau intermédiaire (niveau 2)
         List<Course> recommendedCourses = courseServices.recommendCoursesForUser(2);
+
         assertNotNull(recommendedCourses);
-        assertEquals(1, recommendedCourses.size()); 
+        assertEquals(1, recommendedCourses.size()); // Seulement le cours 1 (débutant) est recommandé
         verify(courseRepository, times(1)).findAll();
     }
 
 
 
-  
+    // Test de la méthode calculateTotalCourseHours
     @Test
-    public void testCalculateTotalCourseHours() {
+     void testCalculateTotalCourseHours() {
         Course course1 = new Course();
         course1.setTimeSlot(3);
 
         Course course2 = new Course();
-        course2.setTimeSlot(5); 
+        course2.setTimeSlot(5);
 
         when(courseRepository.findAll()).thenReturn(Arrays.asList(course1, course2));
 
         int totalHours = courseServices.calculateTotalCourseHours();
 
-        assertEquals(8, totalHours); 
+        assertEquals(8, totalHours); // Vérifie que la durée totale est de 8 heures
         verify(courseRepository, times(1)).findAll();
     }
 
