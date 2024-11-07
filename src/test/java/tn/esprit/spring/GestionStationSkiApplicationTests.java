c45a132848c861af34a237ee35f0b4ee2985e3c1
Dhia aissa <dhia.aissa@esprit.tn>
2024-11-07 23:10:02 +0100

    Add src folder and pom.xml v1.4

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,15 +1,16 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@Lazy // Lazy initialization to avoid eager loading issues
-class GestionStationSkiApplicationTests {
+public class GestionStationSkiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 		// Test application context loading
 	}
 }
